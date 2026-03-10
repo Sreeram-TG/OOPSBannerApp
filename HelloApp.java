738d135accec0f037460945838e01ad5ea590447
Sreeram-TG <st2652@srmist.edu.in>
2026-03-10 13:20:44 +0530

    docs: implement UC1 display hello world

---
+++ HelloApp.java
@@ -0,0 +1,7 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+        // Step 3: App prints "Hello World" to the console
+        System.out.println("Hello World");
+    }
+}
