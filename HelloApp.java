86b6a5674ee570763edb322fda5c0c539ca85c51
Sreeram-TG <st2652@srmist.edu.in>
2026-03-11 13:11:32 +0530

    Delete HelloApp.java

--- HelloApp.java
+++
@@ -1,7 +0,0 @@
-public class HelloApp {
-
-    public static void main(String[] args) {
-        // Step 3: App prints "Hello World" to the console
-        System.out.println("Hello World");
-    }
-}
