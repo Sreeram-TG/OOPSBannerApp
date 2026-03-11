8a2e6d430bcabeffb81606f01fa32bfacfc2d5c3
Sreeram-TG <st2652@srmist.edu.in>
2026-03-11 13:08:54 +0530

    Delete QSN3.java

--- QSN3.java
+++
@@ -1,9 +0,0 @@
-
-public class QSN3 {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double kmToMilesExchange = 1.6;
-        double miles = kilometers / kmToMilesExchange;
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
